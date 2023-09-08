103f1393c87a863debc81b558feb38494c990a2d
Eivour <laragoke91@gmail.com>
2023-09-08 01:24:07 -0700

    Fixed errors

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ *This is the main function of the program.
  *
+ *It returns an integer value to indicate exit status of program
  *
  *return: Always 0 (Success)
  */
-
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art\n");
