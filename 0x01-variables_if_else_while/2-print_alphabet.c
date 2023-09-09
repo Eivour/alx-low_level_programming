b847e166fa07167d34c544e35866c4fb1297dbff
Eivour <laragoke91@gmail.com>
2023-09-08 18:19:56 -0700

    All errors fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-	char alphabet - 'a';
-	while (alphabet <= 'z')
-	{
-		putchar(alphabet);
-		alphabet++;
-	}
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
 	putchar('\n');
+
 	return (0);
 }
