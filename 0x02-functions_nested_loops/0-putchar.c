36cb11bd58acd86b7aebb81fb7ae577a91f87c56
Eivour <laragoke91@gmail.com>
2023-09-13 09:43:48 -0700

    Printed _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,20 +8,17 @@
  *
  * Return: Always 0 (success)
  */
-int main(void)
-{
-	int _putchar(char c)
-	{
-		_putchar('_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
-	}
 
+int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+	}
