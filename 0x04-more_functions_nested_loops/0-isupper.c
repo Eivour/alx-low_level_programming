e075387c08eb1b103c6134fe0a7e967c8b338ed7
Eivour <laragoke91@gmail.com>
2023-09-15 15:52:43 -0700

    Oh welp

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point for code
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isupper(int c);
+{
+	if (c >= 65)
+		return (1);
+
+	else if (c <= 90)
+		return (0);
+}
