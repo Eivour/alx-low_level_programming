9971bd5549d5efbb2952848dab8774929700386d
Eivour <laragoke91@gmail.com>
2023-09-28 19:01:33 -0700

    Length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
