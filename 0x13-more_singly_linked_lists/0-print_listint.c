4f8622195f3ae4aec30bced51e9a5cae527dbefd
Eivour <laragoke91@gmail.com>
2023-10-24 10:06:09 -0700

    What is wrong

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_listint - prints all elements of listint_t list
@@ -11,7 +10,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t length = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 	}
