689462f746f0af0af46ab06792087149743dac75
Eivour <laragoke91@gmail.com>
2023-10-30 18:42:37 -0700

    Fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,28 +12,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, write_status, text_length;
+	int file_descriptor, write_status, text_length;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-        return (-1);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (text_length = 0; text_content[text_length]; text_length++)
-            ;
+	if (text_content != NULL)
+	{
+		for (text_length = 0; text_content[text_length]; text_length++)
+			;
 
-        write_status = write(file_descriptor, text_content, text_length);
-        if (write_status == -1)
-        {
-            close(file_descriptor);
-            return (-1);
-        }
-    }
+		write_status = write(file_descriptor, text_content, text_length);
+		if (write_status == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
 
-    close(file_descriptor);
-    return (1);
+	close(file_descriptor);
+	return (1);
 }
